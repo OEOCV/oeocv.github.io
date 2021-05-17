4cacb46fa14498740baa10c24474800be7804e48
OEOCV_Creator <contact@oeocv.org>
2021-05-17 00:14:34 -0500

    First build

---
+++ build/bible/metadata.js
@@ -0,0 +1,666 @@
+biblename = "Imported Bible";
+metadata = [{
+abbr:"Pre",
+short:"Preface",
+long:"Preface",
+osis:"x-Intr",
+type:"meta",
+nt:false,
+chapters:1
+},{
+abbr:"Genesis",
+short:"Genesis",
+long:"The First Book of Moses, Commonly Called Genesis",
+osis:"Gen",
+type:"ot",
+nt:false,
+chapters:50
+},{
+abbr:"Exodus",
+short:"Exodus",
+long:"The Second Book of Mosis, Commonly Called Exodus",
+osis:"Exod",
+type:"ot",
+nt:false,
+chapters:40
+},{
+abbr:"Leviticus",
+short:"Leviticus",
+long:"The Third Book of Mosis, Commonly Called Leviticus",
+osis:"Lev",
+type:"ot",
+nt:false,
+chapters:27
+},{
+abbr:"Numbers",
+short:"Numbers",
+long:"The Fourth Book of Moses, Commonly Called Numbers",
+osis:"Num",
+type:"ot",
+nt:false,
+chapters:36
+},{
+abbr:"Deuteronomy",
+short:"Deuteronomy",
+long:"The Fifth Book of Moses, Commonly Called Deuteronomy",
+osis:"Deut",
+type:"ot",
+nt:false,
+chapters:34
+},{
+abbr:"Joshua",
+short:"Joshua",
+long:"The Book of Joshua",
+osis:"Josh",
+type:"ot",
+nt:false,
+chapters:24
+},{
+abbr:"Judges",
+short:"Judges",
+long:"The Book of Judges",
+osis:"Judg",
+type:"ot",
+nt:false,
+chapters:21
+},{
+abbr:"Ruth",
+short:"Ruth",
+long:"The Book of Ruth",
+osis:"Ruth",
+type:"ot",
+nt:false,
+chapters:4
+},{
+abbr:"1Samuel",
+short:"1 Samuel",
+long:"The First Book of Samuel",
+osis:"1Sam",
+type:"ot",
+nt:false,
+chapters:31
+},{
+abbr:"2Samuel",
+short:"2 Samuel",
+long:"The Second Book of Samuel",
+osis:"2Sam",
+type:"ot",
+nt:false,
+chapters:24
+},{
+abbr:"1Kings",
+short:"1 Kings",
+long:"The First Book of Kings",
+osis:"1Kgs",
+type:"ot",
+nt:false,
+chapters:22
+},{
+abbr:"2Kings",
+short:"2 Kings",
+long:"The Second Book of Kings",
+osis:"2Kgs",
+type:"ot",
+nt:false,
+chapters:25
+},{
+abbr:"1Chronicles",
+short:"1 Chronicles",
+long:"The First Book of Chronicles",
+osis:"1Chr",
+type:"ot",
+nt:false,
+chapters:29
+},{
+abbr:"2Chronicles",
+short:"2 Chronicles",
+long:"The Second Book of Chronicles",
+osis:"2Chr",
+type:"ot",
+nt:false,
+chapters:36
+},{
+abbr:"Ezra",
+short:"Ezra",
+long:"The Book of Ezra",
+osis:"Ezra",
+type:"ot",
+nt:false,
+chapters:10
+},{
+abbr:"Nehemiah",
+short:"Nehemiah",
+long:"The Book of Nehemiah",
+osis:"Neh",
+type:"ot",
+nt:false,
+chapters:13
+},{
+abbr:"Esther",
+short:"Esther",
+long:"The Book of Esther",
+osis:"Esth",
+type:"ot",
+nt:false,
+chapters:10
+},{
+abbr:"Job",
+short:"Job",
+long:"The Book of Job",
+osis:"Job",
+type:"ot",
+nt:false,
+chapters:42
+},{
+abbr:"Psalm",
+short:"Psalms",
+long:"The Psalms",
+osis:"Ps",
+type:"ot",
+nt:false,
+chapters:150
+},{
+abbr:"Proverbs",
+short:"Proverbs",
+long:"The Proverbs",
+osis:"Prov",
+type:"ot",
+nt:false,
+chapters:31
+},{
+abbr:"Ecclesiastes",
+short:"Ecclesiastes",
+long:"Ecclesiates or, The Preacher",
+osis:"Eccl",
+type:"ot",
+nt:false,
+chapters:12
+},{
+abbr:"SongofSolomon",
+short:"Song of Solomon",
+long:"The Song of Solomon",
+osis:"Song",
+type:"ot",
+nt:false,
+chapters:8
+},{
+abbr:"Isaiah",
+short:"Isaiah",
+long:"The Book of Isaiah",
+osis:"Isa",
+type:"ot",
+nt:false,
+chapters:66
+},{
+abbr:"Jeremiah",
+short:"Jeremiah",
+long:"The Book of Jeremiah",
+osis:"Jer",
+type:"ot",
+nt:false,
+chapters:52
+},{
+abbr:"Lamentations",
+short:"Lamentations",
+long:"The Lamentations of Jeremiah",
+osis:"Lam",
+type:"ot",
+nt:false,
+chapters:5
+},{
+abbr:"Ezekiel",
+short:"Ezekiel",
+long:"The Book of Ezekiel",
+osis:"Ezek",
+type:"ot",
+nt:false,
+chapters:48
+},{
+abbr:"Daniel",
+short:"Daniel",
+long:"The Book of Daniel",
+osis:"Dan",
+type:"ot",
+nt:false,
+chapters:12
+},{
+abbr:"Hosea",
+short:"Hosea",
+long:"The Book of Hosea",
+osis:"Hos",
+type:"ot",
+nt:false,
+chapters:14
+},{
+abbr:"Joel",
+short:"Joel",
+long:"The Book of Joel",
+osis:"Joel",
+type:"ot",
+nt:false,
+chapters:3
+},{
+abbr:"Amos",
+short:"Amos",
+long:"The Book of Amos",
+osis:"Amos",
+type:"ot",
+nt:false,
+chapters:9
+},{
+abbr:"Obadiah",
+short:"Obadiah",
+long:"The Book of Obadiah",
+osis:"Obad",
+type:"ot",
+nt:false,
+chapters:1
+},{
+abbr:"Jonah",
+short:"Jonah",
+long:"The Book of Jonah",
+osis:"Jonah",
+type:"ot",
+nt:false,
+chapters:4
+},{
+abbr:"Micah",
+short:"Micah",
+long:"The Book of Micah",
+osis:"Mic",
+type:"ot",
+nt:false,
+chapters:7
+},{
+abbr:"Nahum",
+short:"Nahum",
+long:"The Book of Nahum",
+osis:"Nah",
+type:"ot",
+nt:false,
+chapters:3
+},{
+abbr:"Habakkuk",
+short:"Habakkuk",
+long:"The Book of Habakkuk",
+osis:"Hab",
+type:"ot",
+nt:false,
+chapters:3
+},{
+abbr:"Zephaniah",
+short:"Zephaniah",
+long:"The Book of Zephaniah",
+osis:"Zeph",
+type:"ot",
+nt:false,
+chapters:3
+},{
+abbr:"Haggai",
+short:"Haggai",
+long:"The Book of Haggai",
+osis:"Hag",
+type:"ot",
+nt:false,
+chapters:2
+},{
+abbr:"Zechariah",
+short:"Zechariah",
+long:"The Book of Zechariah",
+osis:"Zech",
+type:"ot",
+nt:false,
+chapters:14
+},{
+abbr:"Malachi",
+short:"Malachi",
+long:"The Book of Malachi",
+osis:"Mal",
+type:"ot",
+nt:false,
+chapters:4
+},{
+abbr:"Matthew",
+short:"Matthew",
+long:"The Good News According to Matthew",
+osis:"Matt",
+type:"nt",
+nt:true,
+chapters:28
+},{
+abbr:"Mark",
+short:"Mark",
+long:"The Good News According to Mark",
+osis:"Mark",
+type:"nt",
+nt:true,
+chapters:16
+},{
+abbr:"Luke",
+short:"Luke",
+long:"The Good News According to Luke",
+osis:"Luke",
+type:"nt",
+nt:true,
+chapters:24
+},{
+abbr:"John",
+short:"John",
+long:"The Good News According to John",
+osis:"John",
+type:"nt",
+nt:true,
+chapters:21
+},{
+abbr:"Acts",
+short:"Acts",
+long:"The Acts of the Apostles",
+osis:"Acts",
+type:"nt",
+nt:true,
+chapters:28
+},{
+abbr:"Romans",
+short:"Romans",
+long:"Paul’s Letter to the Romans",
+osis:"Rom",
+type:"nt",
+nt:true,
+chapters:16
+},{
+abbr:"1Corinthians",
+short:"1 Corinthians",
+long:"Paul’s First Letter to the Corinthians",
+osis:"1Cor",
+type:"nt",
+nt:true,
+chapters:16
+},{
+abbr:"2Corinthians",
+short:"2 Corinthians",
+long:"Paul’s Second Letter to the Corinthians",
+osis:"2Cor",
+type:"nt",
+nt:true,
+chapters:13
+},{
+abbr:"Galatians",
+short:"Galatians",
+long:"Paul’s Letter to the Galatians",
+osis:"Gal",
+type:"nt",
+nt:true,
+chapters:6
+},{
+abbr:"Ephesians",
+short:"Ephesians",
+long:"Paul’s Letter to the Ephesians",
+osis:"Eph",
+type:"nt",
+nt:true,
+chapters:6
+},{
+abbr:"Philippians",
+short:"Philippians",
+long:"Paul’s Letter to the Philippians",
+osis:"Phil",
+type:"nt",
+nt:true,
+chapters:4
+},{
+abbr:"Colossians",
+short:"Colossians",
+long:"Paul’s Letter to the Colossians",
+osis:"Col",
+type:"nt",
+nt:true,
+chapters:4
+},{
+abbr:"1Thessalonians",
+short:"1 Thessalonians",
+long:"Paul’s First Letter to the Thessalonians",
+osis:"1Thess",
+type:"nt",
+nt:true,
+chapters:5
+},{
+abbr:"2Thessalonians",
+short:"2 Thessalonians",
+long:"Paul’s Second Letter to the Thessalonians",
+osis:"2Thess",
+type:"nt",
+nt:true,
+chapters:3
+},{
+abbr:"1Timothy",
+short:"1 Timothy",
+long:"Paul’s First Letter to Timothy",
+osis:"1Tim",
+type:"nt",
+nt:true,
+chapters:6
+},{
+abbr:"2Timothy",
+short:"2 Timothy",
+long:"Paul’s Second Letter to Timothy",
+osis:"2Tim",
+type:"nt",
+nt:true,
+chapters:4
+},{
+abbr:"Titus",
+short:"Titus",
+long:"Paul’s Letter to Titus",
+osis:"Titus",
+type:"nt",
+nt:true,
+chapters:3
+},{
+abbr:"Philemon",
+short:"Philemon",
+long:"Paul’s Letter to Philemon",
+osis:"Phlm",
+type:"nt",
+nt:true,
+chapters:1
+},{
+abbr:"Hebrews",
+short:"Hebrews",
+long:"The Letter to the Hebrews",
+osis:"Heb",
+type:"nt",
+nt:true,
+chapters:13
+},{
+abbr:"James",
+short:"James",
+long:"The Letter from James",
+osis:"Jas",
+type:"nt",
+nt:true,
+chapters:5
+},{
+abbr:"1Peter",
+short:"1 Peter",
+long:"Peter’s First Letter",
+osis:"1Pet",
+type:"nt",
+nt:true,
+chapters:5
+},{
+abbr:"2Peter",
+short:"2 Peter",
+long:"Peter’s Second Letter",
+osis:"2Pet",
+type:"nt",
+nt:true,
+chapters:3
+},{
+abbr:"1John",
+short:"1 John",
+long:"John’s First Letter",
+osis:"1John",
+type:"nt",
+nt:true,
+chapters:5
+},{
+abbr:"2John",
+short:"2 John",
+long:"John’s Second Letter",
+osis:"2John",
+type:"nt",
+nt:true,
+chapters:1
+},{
+abbr:"3John",
+short:"3 John",
+long:"John’s Third Letter",
+osis:"3John",
+type:"nt",
+nt:true,
+chapters:1
+},{
+abbr:"Jude",
+short:"Jude",
+long:"The Letter from Jude",
+osis:"Jude",
+type:"nt",
+nt:true,
+chapters:1
+},{
+abbr:"Revelation",
+short:"Revelation",
+long:"The Revelation to John",
+osis:"Rev",
+type:"nt",
+nt:true,
+chapters:22
+},{
+abbr:"Tobit",
+short:"Tobit",
+long:"Tobit",
+osis:"Tob",
+type:"ot",
+nt:false,
+chapters:14
+},{
+abbr:"Judith",
+short:"Judith",
+long:"Judith",
+osis:"Jdt",
+type:"ot",
+nt:false,
+chapters:16
+},{
+abbr:"ESG",
+short:"Esther (Greek)",
+long:"Esther (Greek)",
+osis:"EsthGr",
+type:"ot",
+nt:false,
+chapters:10
+},{
+abbr:"Wisdom",
+short:"Wisdom of Solomon",
+long:"The Wisdom of Solomon",
+osis:"Wis",
+type:"ot",
+nt:false,
+chapters:19
+},{
+abbr:"Sirach",
+short:"Sirach",
+long:"The Wisdom of Jesus the Son of Sirach, or Ecclesiasticus",
+osis:"Sir",
+type:"ot",
+nt:false,
+chapters:51
+},{
+abbr:"Baruch",
+short:"Baruch",
+long:"Baruch",
+osis:"Bar",
+type:"ot",
+nt:false,
+chapters:6
+},{
+abbr:"1Ma",
+short:"1 Maccabees",
+long:"The First Book of the Maccabees",
+osis:"1Macc",
+type:"ot",
+nt:false,
+chapters:16
+},{
+abbr:"2Ma",
+short:"2 Maccabees",
+long:"The Second Book of the Maccabees",
+osis:"2Macc",
+type:"ot",
+nt:false,
+chapters:15
+},{
+abbr:"3Ma",
+short:"3 Maccabees",
+long:"The Third Book of the Maccabees",
+osis:"3Macc",
+type:"ot",
+nt:false,
+chapters:7
+},{
+abbr:"4Ma",
+short:"4 Maccabees",
+long:"The Fourth Book of the Maccabees",
+osis:"4Macc",
+type:"ot",
+nt:false,
+chapters:18
+},{
+abbr:"1Es",
+short:"1 Esdras",
+long:"1 Esdras",
+osis:"1Esd",
+type:"ot",
+nt:false,
+chapters:9
+},{
+abbr:"2Es",
+short:"2 Esdras",
+long:"2 Esdras",
+osis:"2Esd",
+type:"ot",
+nt:false,
+chapters:16
+},{
+abbr:"Man",
+short:"Prayer of Manasses",
+long:"The Prayer of Manasses King of Judah when He was Held Captive in Babylon",
+osis:"PrMan",
+type:"ot",
+nt:false,
+chapters:1
+},{
+abbr:"Ps151",
+short:"Psalm 151",
+long:"Psalm 151",
+osis:"AddPs",
+type:"ot",
+nt:false,
+chapters:1
+},{
+abbr:"DAG",
+short:"Daniel (Greek)",
+long:"The Book of Daniel with Greek Portions",
+osis:"DanGr",
+type:"ot",
+nt:false,
+chapters:14
+},{
+abbr:"Glo",
+short:"Glossary",
+long:"Open Eastern Orthodox Commentary Version Glossary",
+osis:"x-App-Gloss",
+type:"meta",
+nt:true,
+chapters:1
+}];
