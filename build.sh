79c360fe0c7e4e1f30f891ca2805a16ff7872eb4
OEOCV_Creator <contact@oeocv.org>
2021-05-19 15:22:38 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,6 +8,8 @@ then
 	unzip BibleMultiConverter-0.0.7.zip -d BibleMultiConverter-0.0.7
 fi
 
+# TODO: Add OEOCV-Tools as a submodule and require the books to be validated before they are built
+
 java -jar ./BibleMultiConverter-0.0.7/BibleMultiConverter.jar USFM ./OEOCV/source/ RoundtripHTML ./docs/bible
 
 echo "Done"
